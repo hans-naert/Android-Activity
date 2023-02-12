4c99f73de4be15ee667c78ab97c4ab0377a8e579
Hans Naert <hans.naert@vives.be>
2023-02-12 21:46:30 +0100

    add implicit and explicit intents

--- app/src/main/java/com/example/androidactivity/FirstActivity.kt
+++ app/src/main/java/com/example/androidactivity/FirstActivity.kt
@@ -1,6 +1,11 @@
 package com.example.androidactivity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidactivity.databinding.ActivityFirstBinding
@@ -12,13 +17,41 @@ class FirstActivity : AppCompatActivity() {
         binding = ActivityFirstBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //setContentView(R.layout.activity_first)
-        //val button1: Button =findViewById(R.id.button1)
-        with(binding)
-        {
+        val button1: Button =findViewById(R.id.button1)
+        //with(binding)
+        //{
             button1.setOnClickListener {
-                Toast.makeText(this@FirstActivity, "You clicked Button 1", Toast.LENGTH_SHORT)
-                    .show()
+                //val intent = Intent(this@FirstActivity, SecondActivity::class.java)
+                //val intent = Intent("com.example.androidactivity.ACTION_START")
+                //intent.addCategory("com.example.androidactivity.MY_CATEGORY")
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data=Uri.parse("http://www.vives.be")
+                //intent.setType("text/html")
+                startActivity(intent)
+                /*intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(Intent.createChooser(intent, "Choose browser"))
+                } else {
+                    // No activities can handle the intent
+                    Log.v("Intent","No activities can handle the intent")
+                }*/
             }
+        //}
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "You clicked Add",
+                Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "You clicked Remove",
+                Toast.LENGTH_SHORT).show()
         }
+        return true
     }
 }

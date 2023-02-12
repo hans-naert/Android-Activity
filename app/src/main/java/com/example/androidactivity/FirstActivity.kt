308872d186ebb68defc8ac25a869417b798dbfda
Hans Naert <hans.naert@vives.be>
2023-02-12 19:32:15 +0100

    add Toast using viewbinding on button1

--- app/src/main/java/com/example/androidactivity/FirstActivity.kt
+++ app/src/main/java/com/example/androidactivity/FirstActivity.kt
@@ -1,11 +1,24 @@
 package com.example.androidactivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidactivity.databinding.ActivityFirstBinding
 
 class FirstActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFirstBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.first_layout)
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_first)
+        //val button1: Button =findViewById(R.id.button1)
+        with(binding)
+        {
+            button1.setOnClickListener {
+                Toast.makeText(this@FirstActivity, "You clicked Button 1", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 }

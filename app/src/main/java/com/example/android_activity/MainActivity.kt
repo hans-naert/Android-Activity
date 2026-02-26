4698f32c9bae065cd8dbbbc79095060ccf50bd75
Hans Naert <hans.naert@vives.be>
2026-02-26 15:14:38 +0100

    Add button and show Toast

---
+++ app/src/main/java/com/example/android_activity/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.android_activity
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            // Handle button click
+            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
